cee66b341b26cebd98fe49afc28d5d4185013d06
ssssujini99 <sssssjin99@naver.com>
2022-08-07 01:58:30 +0900

    Chore(main/views.py): 주석 제거

--- main/views.py
+++ main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-#
-# from rest_framework import permissions
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-#
-# # Swagger test용 - 이후 삭제
-# class TestView(APIView):
-#     permission_classes = [permissions.AllowAny]
-#
-#     def get(self, request):
-#         return Response("Swagger 연동 테스트")
+
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Swagger test용 - 이후 삭제
+class TestView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        return Response("Swagger 연동 테스트")
