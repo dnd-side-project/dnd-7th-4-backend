0db366bef8fd74fff7e0fe4754000d011f49ec20
chea-young <63410509+chea-young@users.noreply.github.com>
2022-08-09 20:28:37 +0900

    Feat(main/../api_6.py): add timeouterror, connectionerror

--- main/api_callback_directory/api_6.py
+++ main/api_callback_directory/api_6.py
@@ -28,35 +28,40 @@ def call_api_6():
             "numOfRows": 300,
             "ver": "1.3",
         }
-        # api 요청
-        response = requests.get(url, params=params)
+        
+        try:
+            # api 요청
+            response = requests.get(url, params=params)
+            print(f'api_6: get request: {local}-----------------------------')
 
-        print(f'api_6: get request: {local}-----------------------------')
+            # 데이터 받기가 성공일 경우
+            if response.status_code == 200:
+                for item in response.json()['response']['body']['items']:
+                    # API6 model에 저장
+                    sido_name = item['sidoName']
+                    station_name = item['stationName']
+                    pm_data = [item['pm10Grade1h'], item['pm25Grade1h'], item['pm10Value24'], item['pm25Value24']]
 
-        # 데이터 받기가 성공일 경우
-        if response.status_code == 200:
-            for item in response.json()['response']['body']['items']:
-                # API6 model에 저장
-                sido_name = item['sidoName']
-                station_name = item['stationName']
-                pm_data = [item['pm10Grade1h'], item['pm25Grade1h'], item['pm10Value24'], item['pm25Value24']]
+                    # 측정 불가 데이터 처리
+                    for i in range(4):
+                        if pm_data[i] == '-':
+                            pm_data[i] = 0
 
-                # 측정 불가 데이터 처리
-                for i in range(4):
-                    if pm_data[i] == '-':
-                        pm_data[i] = 0
-
-                print(f'api_6: {sido_name} {station_name} -----------------------------')
-                api6 = Api_6(sidoName = sido_name, stationName = station_name, pm10Grade1h = pm_data[0], pm25Grade1h = pm_data[1],
-                            pm10Value24 = pm_data[2], pm25Value24 = pm_data[3])
-                api6.save()
-                
-                # Region에 FK 연결
-                region_data = Region.objects.filter(api6_station = station_name)
-                for region in region_data:
-                    print(f'api_6: connect fk {region} {api6.id} -----------------------------')
-                    region.api6_id = api6
-                    region.save()
+                    print(f'api_6: {sido_name} {station_name} -----------------------------')
+                    api6 = Api_6(sidoName = sido_name, stationName = station_name, pm10Grade1h = pm_data[0], pm25Grade1h = pm_data[1],
+                                pm10Value24 = pm_data[2], pm25Value24 = pm_data[3])
+                    api6.save()
+                    
+                    # Region에 FK 연결
+                    region_data = Region.objects.filter(api6_station = station_name)
+                    for region in region_data:
+                        print(f'api_6: connect fk {region} {api6.id} -----------------------------')
+                        region.api6_id = api6
+                        region.save()
+        except requests.Timeout:
+            print(f'api_6: Timeout: {local}-----------------------------')
+        except requests.ConnectionError:
+            print(f'api_6: ConnectionError: {local}-----------------------------')
 
 # 1시간 주기로 api_6 데이터 업데이트
 def update_api_6():
@@ -77,30 +82,36 @@ def update_api_6():
             "numOfRows": 300,
             "ver": "1.3",
         }
-        # api 요청
-        response = requests.get(url, params=params)
-        print(f'api_6: get request: {local}-----------------------------')
+        
+        try:
+            # api 요청
+            response = requests.get(url, params=params)
+            print(f'api_6: get request: {local}-----------------------------')
 
-        # 데이터 받기가 성공일 경우
-        if response.status_code == 200:
-            for item in response.json()['response']['body']['items']:
-                # API6 model에 저장
-                sido_name = item['sidoName']
-                station_name = item['stationName']
-                pm_data = [item['pm10Grade1h'], item['pm25Grade1h'], item['pm10Value24'], item['pm25Value24']]
+            # 데이터 받기가 성공일 경우
+            if response.status_code == 200:
+                for item in response.json()['response']['body']['items']:
+                    # API6 model에 저장
+                    sido_name = item['sidoName']
+                    station_name = item['stationName']
+                    pm_data = [item['pm10Grade1h'], item['pm25Grade1h'], item['pm10Value24'], item['pm25Value24']]
 
-                # 측정 불가 데이터 처리
-                for i in range(4):
-                    if pm_data[i] == '-':
-                        pm_data[i] = 0
+                    # 측정 불가 데이터 처리
+                    for i in range(4):
+                        if pm_data[i] == '-':
+                            pm_data[i] = 0
 
-                print(f'api_6: {sido_name} {station_name} -----------------------------')
+                    print(f'api_6: {sido_name} {station_name} -----------------------------')
 
-                # api 데이터 찾아서 업데이트
-                api6_data = Api_6.objects.filter(stationName = station_name)
-                for api6 in api6_data:
-                    api6.pm10Grade1h = pm_data[0]
-                    api6.pm25Grade1h = pm_data[1]
-                    api6.pm10Value24 = pm_data[2]
-                    api6.pm25Value24 = pm_data[3]
-                    api6.save()
+                    # api 데이터 찾아서 업데이트
+                    api6_data = Api_6.objects.filter(stationName = station_name)
+                    for api6 in api6_data:
+                        api6.pm10Grade1h = pm_data[0]
+                        api6.pm25Grade1h = pm_data[1]
+                        api6.pm10Value24 = pm_data[2]
+                        api6.pm25Value24 = pm_data[3]
+                        api6.save()
+        except requests.Timeout:
+            print(f'api_6: Timeout: {local}-----------------------------')
+        except requests.ConnectionError:
+            print(f'api_6: ConnectionError: {local}-----------------------------')
