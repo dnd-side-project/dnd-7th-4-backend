42dfe3919d0c940ec1a7e84bf3bed720e9c4f6d1
chea-young <63410509+chea-young@users.noreply.github.com>
2022-08-11 21:44:23 +0900

    Update(tasks.py) : add api9 in tasks

--- main/tasks.py
+++ main/tasks.py
@@ -10,7 +10,8 @@ from main.api_callback_directory.api_1 import func1, func2
 from main.api_callback_directory.api_6 import call_api_6, update_api_6
 from main.api_callback_directory.api_7 import call_api_7, update_api_7
 from main.api_callback_directory.api_8 import call_api_8, update_api_8
-from main.models import Region, Api_6, Api_7, Api8
+from main.api_callback_directory.api_9 import call_api_9, update_api_9
+from main.models import Region, Api_6, Api_7, Api8, Api9
 
 
 # test 용 함수
@@ -58,3 +59,13 @@ def api_8():
     else:
         print('api_8: update -----------------------------')
         update_api_8()
+
+@shared_task
+def api_9():
+    if not len(Api9.objects.all()): # api_9 가 비어있는 경우
+        print('api_9: save -----------------------------')
+        call_api_9()
+
+    else:
+        print('api_9: update -----------------------------')
+        update_api_9()
