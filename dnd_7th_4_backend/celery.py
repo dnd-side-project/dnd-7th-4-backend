d12eeef5c4dd172fa5db74cc051d6d541cd4b499
ssssujini99 <sssssjin99@naver.com>
2022-08-13 19:09:42 +0900

    :poop: Test(celery.py): error checking

--- dnd_7th_4_backend/celery.py
+++ dnd_7th_4_backend/celery.py
@@ -44,24 +44,24 @@ app.conf.beat_schedule = {
         'schedule': crontab(minute=0, hour='6,18'),  # 6시, 18시에 호출
     },
 
-    'add-every-60-minutes-api6': {
-        'task': 'main.tasks.api_6',
-        'schedule': crontab(minute='*/60'),
-
-    },
-    'add-every-24-hour-api7': {
-        'task': 'main.tasks.api_7',
-        'schedule': crontab(minute=0, hour=0),
-
-    },
-    'add-every-at-06-api8': {
-        'task': 'main.tasks.api_8',
-        'schedule': crontab(hour='6'),
-    },
-    
-    'add-every-at-06-18-api9': {
-        'task': 'main.tasks.api_9',
-        'schedule': crontab(hour='6, 18'),
-    },
+    # 'add-every-60-minutes-api6': {
+    #     'task': 'main.tasks.api_6',
+    #     'schedule': crontab(minute='*/60'),
+    #
+    # },
+    # 'add-every-24-hour-api7': {
+    #     'task': 'main.tasks.api_7',
+    #     'schedule': crontab(minute=0, hour=0),
+    #
+    # },
+    # 'add-every-at-06-api8': {
+    #     'task': 'main.tasks.api_8',
+    #     'schedule': crontab(hour='6'),
+    # },
+    #
+    # 'add-every-at-06-18-api9': {
+    #     'task': 'main.tasks.api_9',
+    #     'schedule': crontab(hour='6, 18'),
+    # },
 
 } 
