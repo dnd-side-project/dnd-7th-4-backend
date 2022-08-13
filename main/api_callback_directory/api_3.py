5e3c874f3bbffed9594e4b6f0c4baa095440e462
ssssujini99 <sssssjin99@naver.com>
2022-08-14 02:12:47 +0900

    :bug: Fix: update test code

--- main/api_callback_directory/api_3.py
+++ main/api_callback_directory/api_3.py
@@ -21,6 +21,8 @@ def func3():
     base_date = current.strftime("%Y%m%d")
     base_time = current.strftime("%H%M") ## 배포용
     # base_time = "1715"  # 테스트용
+    base_date = "20220813"
+    base_time = "2315"
 
     for i in range(1, 251):  # region id: 1 ~ 250 까지 정보 업데이트
         region = Region.objects.get(id=i)
