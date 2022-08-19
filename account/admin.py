62c6fd70a74ac433e0124aa9d5a42d75580f2aa2
ssssujini99 <sssssjin99@naver.com>
2022-08-20 00:25:51 +0900

    :truck: Chore: User_Region 모델 관련 파일 이동

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import *
 
 admin.site.register(Profile)
+
+
+@admin.register(User_Region)
+class Api1Admin(admin.ModelAdmin):
+    list_display = [field.name for field in User_Region._meta.get_fields()]
