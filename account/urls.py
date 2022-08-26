8eb36feaba225f3431f289155639e4344a20ab31
ssssujini99 <sssssjin99@naver.com>
2022-08-27 00:18:55 +0900

    :white_check_mark: Update: local test 추가

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('kakao', views.front),
+    path('kakao', views.front),
     path('kakao/oauth', views.back),
     path('kakao/logout', views.logout),
 
